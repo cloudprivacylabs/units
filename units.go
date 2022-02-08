f7e213f904d34b4f07e967d328a9604265c7563c
Jason Cruz <jac024@ucsd.edu>
2022-02-08 15:12:48 -0800

    regexp

--- units.go
+++ units.go
@@ -1,17 +1,24 @@
 package units
 
 import (
-	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 )
+
+type ErrAmbiguousUnit []string
+
+func (e ErrAmbiguousUnit) Error() string {
+	return "Ambiguous unit: " + strings.Join([]string(e), ", ")
+}
 
 type UnitRegex struct {
 	Regex     *regexp.Regexp
 	Converter func(matches []string) (value, unit string)
+	UnitName  string
 }
 
-var HintedRegex = make(map[string][]UnitRegex, 0)
+var hintedRegex = make(map[string][]UnitRegex, 0)
 
 func init() {
 	RegisterUnixRegex(`(?P<ft>[[:digit:]]+)\'(?P<in>[[:digit:]]+)\"`, ParseFeetInch, "[in_i]", "height", "length")
@@ -20,39 +27,42 @@ func init() {
 func RegisterUnixRegex(regex string, converter func(matches []string) (value, unit string),
 	unitName string, hints ...string) {
 	re := regexp.MustCompile(regex)
-	ur := UnitRegex{Regex: re}
+	ur := UnitRegex{Regex: re, Converter: converter, UnitName: unitName}
 	for _, hint := range hints {
-		HintedRegex[hint] = append(HintedRegex[hint], ur)
-		HintedRegex[""] = append(HintedRegex[""], ur)
+		hintedRegex[hint] = append(hintedRegex[hint], ur)
 	}
+	hintedRegex[""] = append(hintedRegex[""], ur)
 }
 
-func ParseUnits(in string, hint ...string) (value, unit string) {
+func ParseUnits(in string, hint ...string) (value, unit string, err error) {
 	for _, ht := range hint {
-		for _, unitRegex := range HintedRegex[ht] {
+		for _, unitRegex := range hintedRegex[ht] {
 			matches := unitRegex.Regex.FindAllStringSubmatch(in, -1)
 			if len(matches) == 0 {
 				continue
 			}
-			switch {
-			case ht == "length":
-				unitRegex.Converter = ParseFeetInch
-			case ht == "height":
-				unitRegex.Converter = ParseFeetInch
-			}
-			return unitRegex.Converter(matches[0][:])
+			value, unit = unitRegex.Converter(matches[0])
+			return value, unit, nil
 		}
 	}
+	var ambUnits ErrAmbiguousUnit
 	if len(hint) == 0 {
-		for _, unitRegex := range HintedRegex[""] {
+		for _, unitRegex := range hintedRegex[""] {
 			matches := unitRegex.Regex.FindAllStringSubmatch(in, -1)
 			if len(matches) == 0 {
 				continue
+			} else if len(matches) > 1 {
+				switch {
+				case unitRegex.Regex.String() == `(?P<ft>[[:digit:]]+)\'(?P<in>[[:digit:]]+)\"`:
+					ambUnits = append(ambUnits, unitRegex.UnitName)
+				}
+			} else {
+				value, unit = unitRegex.Converter(matches[0])
+				return value, unit, nil
 			}
-			fmt.Println("unit:", matches[0][0], "-> AMBIGUOUS")
 		}
 	}
-	return value, ""
+	return value, "", ambUnits
 }
 
 func ParseFeetInch(matches []string) (value, unit string) {
