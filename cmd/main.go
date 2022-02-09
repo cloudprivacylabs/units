448ee28c01754a86c91cc7be5c0be3feebea1b46
Jason Cruz <jac024@ucsd.edu>
2022-02-09 08:30:03 -0800

    regexp

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/cloudprivacylabs/units"
 )
 
 func main() {
-	x, y, z := units.ParseUnits(`5'4"`)
-	// x, y = units.ParseUnits(`5'4"`, "length")
-	// x, y = units.ParseUnits(`5'4"`, "height")
+	var cfg config
+	flag.StringVar(&cfg.unit, "unit", "", "define unit")
+	flag.StringVar(&cfg.hint, "hint", "", "define hint")
+	flag.Parse()
+	x, y, z := units.ParseUnits(cfg.unit)
 	fmt.Println(x, y, z)
+}
+
+type config struct {
+	unit string
+	hint string
 }
